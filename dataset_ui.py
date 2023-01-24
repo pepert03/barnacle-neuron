5744702705ccc42bc1ba233a47111a95dc1e7b70
peperidruejo <202111246@alu.comillas.edu>
2023-01-24 09:10:14 +0100

    dataset_ui and data folder

--- dataset_ui.py
+++ dataset_ui.py
@@ -1,5 +1,4 @@
 import os
-import random as rd
 import pygame as pg
 import pandas as pd
 import numpy as np
@@ -7,37 +6,35 @@ import matplotlib.pyplot as plt
 
 
 pg.init()
+RESOLUTION = 28
+INCREASE = 10
+screen = pg.display.set_mode((RESOLUTION*INCREASE,RESOLUTION*INCREASE))
+pg.display.set_caption("Dataset UI")
 
-
-L = 28
-WIN = pg.display.set_mode((L*10,L*10))
-pg.display.set_caption("Numbers")
-print(rd.randint(0, 9))
-
-def draw_gameboard(board):
-    for y in range(L):
-        for x in range(L):
+def draw_grid(board):
+    for y in range(RESOLUTION):
+        for x in range(RESOLUTION):
             n = board[y][x]
-            pg.draw.rect(WIN, (int(255*n),int(255*n),int(255*n)), (x*10, y*10, 10, 10))
+            pg.draw.rect(screen, (int(255*n),int(255*n),int(255*n)), (x*INCREASE, y*INCREASE, INCREASE, INCREASE))
 
 
 def save(export, board):
     '''Save image:
-    Save the pygame window as an image in the data folder'''
-    WIN = pg.display.set_mode((L,L))
-    for y in range(L):
-        for x in range(L):
+    Save the pygame screendow as an image in the data folder'''
+    screen = pg.display.set_mode((RESOLUTION,RESOLUTION))
+    for y in range(RESOLUTION):
+        for x in range(RESOLUTION):
             n = board[y][x]
-            pg.draw.rect(WIN, (int(255*n),int(255*n),int(255*n)), (x, y, 1, 1))    
+            pg.draw.rect(screen, (int(255*n),int(255*n),int(255*n)), (x, y, 1, 1))    
 
 
     if not os.path.exists("data"):
         os.mkdir("data")
     if not os.path.exists("data/{}".format(export)):
         os.mkdir("data/{}".format(export))
-    for i in range(100):
+    for i in range(200):
         if not os.path.exists("data/{}/{}.png".format(export, i)):
-            pg.image.save(WIN, "data/{}/{}.png".format(export, i))
+            pg.image.save(screen, "data/{}/{}.png".format(export, i))
             break
 
 
@@ -66,8 +63,8 @@ def save_image(path,n):
 
 def main():
     '''Main function:
-    Create a window to draw numbers and save them in the data folder'''
-    board = [[0 for x in range(L)] for y in range(L)]
+    Create a screendow to draw numbers and save them in the data folder'''
+    board = [[0 for x in range(RESOLUTION)] for y in range(RESOLUTION)]
     clock = pg.time.Clock()
     export = -1
     run = True
@@ -77,59 +74,42 @@ def main():
             if event.type == pg.QUIT:
                 run = False
             if event.type == pg.KEYUP:
-                if event.key == pg.K_0:
-                    export = 0
-                if event.key == pg.K_1:
-                    export = 1
-                if event.key == pg.K_2:
-                    export = 2
-                if event.key == pg.K_3:
-                    export = 3
-                if event.key == pg.K_4:
-                    export = 4
-                if event.key == pg.K_5:
-                    export = 5
-                if event.key == pg.K_6:
-                    export = 6
-                if event.key == pg.K_7:
-                    export = 7
-                if event.key == pg.K_8:
-                    export = 8
-                if event.key == pg.K_9:
-                    export = 9
+                for i in range(10):
+                    if event.key == pg.K_0 + i:
+                        export = i
         mouse = pg.mouse.get_pressed()
         if mouse[0]:
             x, y = pg.mouse.get_pos()
-            x , y = x//10, y//10
+            x , y = x//INCREASE, y//INCREASE
             board[y][x] = 1
-            if x < L-1:
+            if x < RESOLUTION-1:
                 board[y][x+1] = (1 + board[y][x+1])/2
             if x > 0:
                 board[y][x-1] = (1 + board[y][x-1])/2
-            if y < L-1:
+            if y < RESOLUTION-1:
                 board[y+1][x] = (1 + board[y+1][x])/2
             if y > 0:
                 board[y-1][x] = (1 + board[y-1][x])/2
         if mouse[2]:
             x, y = pg.mouse.get_pos()
-            x , y = x//10, y//10
+            x , y = x//INCREASE, y//INCREASE
             board[y][x] = 0
-            if x < L-1:
+            if x < RESOLUTION-1:
                 board[y][x+1] = 0
             if x > 0:
                 board[y][x-1] = 0
-            if y < L-1:
+            if y < RESOLUTION-1:
                 board[y+1][x] = 0
             if y > 0:
                 board[y-1][x] = 0
         
         if export != -1:
             save(export, board)
-            WIN = pg.display.set_mode((L*10,L*10))
-            board = [[0 for x in range(L)] for y in range(L)]
+            screen = pg.display.set_mode((RESOLUTION*INCREASE,RESOLUTION*INCREASE))
+            board = [[0 for x in range(RESOLUTION)] for y in range(RESOLUTION)]
             export = -1
         
-        draw_gameboard(board)
+        draw_grid(board)
         pg.display.update()
 
     pg.quit()
