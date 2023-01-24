e1c51ef9f22d21cad8d6e362ec91f5cb69fb43aa
peperidruejo <202111246@alu.comillas.edu>
2023-01-24 09:29:56 +0100

    dataset_ui and data folder

---
+++ neunet.py
@@ -0,0 +1,20 @@
+import numpy as np
+import random as r
+import pandas as pd
+
+class NeuNet:
+    def __init__(self) -> None:
+        pass
+
+class Layer:
+    def __init__(self) -> None:
+        self.weights = np.array([])
+        self.biases = np.array([])
+    
+    def forward(self, input):
+        pass
+
+    def backward(self, input, error):
+        pass
+
+
